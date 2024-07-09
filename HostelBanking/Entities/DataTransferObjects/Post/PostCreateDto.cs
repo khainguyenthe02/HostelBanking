2d4b9601d31601b6a4ca7708217a661c31fda949
HuyVuong <vuonghuy12345@gmail.com>
2024-07-09 23:56:25 +0700

    add zalo and street create post dto

--- HostelBanking/Entities/DataTransferObjects/Post/PostCreateDto.cs
+++ HostelBanking/Entities/DataTransferObjects/Post/PostCreateDto.cs
@@ -13,7 +13,9 @@ namespace HostelBanking.Entities.DataTransferObjects.Post
 		public string[] Images { get; set; }
 		public DateTime CreateDate { get; set; }
 		public string PhoneNumber { get; set; }
-		public string OwnerHouse { get; set; }
+        public string Zalo { get; set; }
+        public string Street { get; set; }
+        public string OwnerHouse { get; set; }
 		public DateTime ModifiedDate { get; set; }
 		public int PaymentType { get; set; }
 		public int HostelTypeId { get; set; }
