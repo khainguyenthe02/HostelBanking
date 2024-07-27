46f6fb8126e421b03a843886a28f19ebec1fe7d9
Nguyen Khai <ngthekhai@huyvq.onmicrosoft.com>
2024-07-27 15:53:03 +0700

    add search report by post title and check create report

--- HostelBanking/Controllers/ReportController.cs
+++ HostelBanking/Controllers/ReportController.cs
@@ -1,6 +1,8 @@
 ﻿using HostelBanking.Entities.Const;
 using HostelBanking.Entities.DataTransferObjects.Comment;
 using HostelBanking.Entities.DataTransferObjects.Favorite;
+using HostelBanking.Entities.DataTransferObjects.PayHistory;
+using HostelBanking.Entities.DataTransferObjects.Post;
 using HostelBanking.Entities.DataTransferObjects.Report;
 using HostelBanking.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
@@ -40,10 +42,64 @@ namespace HostelBanking.Controllers
 			{
 				return BadRequest(MessageError.UserOrPostNotExist);
 			}
+			var reportSearch = new ReportSearchDto
+			{
+				PostId = postId,
+				AccountId = report.AccountId
+			};
+			var reportLast = (await _serviceManager.ReportService.Search(reportSearch)).LastOrDefault();
+			if(reportLast != null)
+			{
+				var now = DateTime.Now;
+				if(now < reportLast.CreateDate.AddHours(24))
+				{
+					return BadRequest(MessageError.UserHasReportedYet + reportLast.CreateDate+ ". " + MessageError.ReportLessThan24h);
+				}
+			}
 			var result = await _serviceManager.ReportService.Create(report);
 			if (result) return Ok(result);
+			return BadRequest(MessageError.ErrorCreate);
+		}
+		[HttpPost("search-report-by-post-title")]
+		public async Task<IActionResult> SearchPostByTitle([FromBody] ReportFeatPostTitleDto search, CancellationToken cancellationToken)
+		{
+			List<ReportDto> result = new();
+			List<PostDto> resultPost = new();
 
-			return BadRequest(MessageError.ErrorCreate + ":" + result);
+			if (search != null)
+			{
+				var post = new PostSearchDto
+				{
+					Title = search.PostTitle,
+				};
+				resultPost = await _serviceManager.PostService.Search(post);
+				var report = new ReportSearchDto
+				{
+					ReportStatus = search.ReportStatus,
+					AccountId = search.AccountId,
+				};
+				result = await _serviceManager.ReportService.Search(report);
+			}
+			result = result.Where(ph => resultPost.Any(rp => rp.Id == ph.PostId)).ToList();
+			var count = result.Count();
+			if (count > 0)
+			{
+				var pageIndex = search.PageNumber;
+				int pageSize = (int)search.PageSize;
+				var numberPage = Math.Ceiling((float)(count / pageSize));
+				int start = (pageIndex - 1) * pageSize;
+				var post = result.Skip(start).Take(pageSize);
+				return Ok(new
+				{
+					data = post,
+					totalItem = result.Count,
+					numberPage,
+					search.PageNumber,
+					search.PageSize
+				});
+			}
+			if (result == null) return Ok(new List<ReportDto>());
+			return Ok(result);
 		}
 		[HttpPost("search")]
 		public async Task<IActionResult> SearchDevice([FromBody] ReportSearchDto search, CancellationToken cancellationToken)
---
+++ HostelBanking/Entities/DataTransferObjects/Report/ReportFeatPostTitleDto.cs
@@ -0,0 +1,11 @@
+﻿namespace HostelBanking.Entities.DataTransferObjects.Report
+{
+	public class ReportFeatPostTitleDto
+	{
+		public string? PostTitle { get; set; }
+		public int? ReportStatus { get; set; }
+		public int? AccountId { get; set; }
+		public int PageNumber { get; set; } = 1;
+		public int PageSize { get; set; } = 10;
+	}
+}
